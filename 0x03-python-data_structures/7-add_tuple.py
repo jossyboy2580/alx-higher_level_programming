4ebc8a4910b3187bbeb8be45cecaa66ab9c78254
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-18 13:09:25 +0100

    Adding two tuples together

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    if len(tuple_a) > len(tuple_b):
+        max_len = len(tuple_a)
+    else:
+        max_len = len(tuple_b)
+    for i in range(max_len):
+        elem1 = 0
+        elem2 = 0
+        if i <= len(tuple_a):
+            elem1 = tuple_a[i]
+        if i <= len(tuple_b):
+            elem2 = tuple_b[i]
+        new_tuple = new_tuple , (elem1 + elem2)
+    return (new_tuple)
