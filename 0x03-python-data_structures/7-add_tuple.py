afe7f7153241759cf10b34bbdc53e05cc0207a52
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-18 13:17:29 +0100

    Adding the first two values of a tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
-    if len(tuple_a) > len(tuple_b):
-        max_len = len(tuple_a)
-    else:
-        max_len = len(tuple_b)
-    for i in range(max_len):
+    for i in range(2):
         elem1 = 0
         elem2 = 0
-        if i <= len(tuple_a):
+        if i <= len(tuple_a) - 1:
             elem1 = tuple_a[i]
-        if i <= len(tuple_b):
+        if i <= len(tuple_b) - 1:
             elem2 = tuple_b[i]
-        new_tuple = new_tuple , (elem1 + elem2)
+        new_tuple = new_tuple + ((elem1 + elem2),)
     return (new_tuple)
