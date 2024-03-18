50262043613e006cff0fc9d6579d338c0457f7c5
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-18 21:25:20 +0100

    A linked list palindrome checker in c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,31 @@
 #include <stdlib.h>
+#include "lists.h"
 
 int is_palindrome(listint_t **head)
 {
-	listint_t **nodes_list = NULL;
+	int *values = NULL;
+	size_t i;
+	listint_t *current = *head;
+	size_t values_count = 0;
+
+	while (current)
+	{
+		values = realloc(values, sizeof(int) * ++values_count);
+		if (!values)
+		{
+			return (0);
+		}
+		values[values_count - 1] = current->n;
+		current = current->next;
+	}
+	for (i = 0; i < values_count / 2; i++)
+	{
+		if (values[i] != values[values_count - i - 1])
+		{
+			free(values);
+			return (0);
+		}
+	}
+	free(values);
+	return (1);
 }
