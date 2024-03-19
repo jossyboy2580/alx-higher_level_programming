8a7cdc8e74501ddeb47fd3cf78b2cc8eb09d9cee
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-19 12:40:39 +0100

    remove element from list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     listy = list(my_string)
     len_listy = len(listy)
-    for i in range(len_listy:
+    for i in range(len_listy):
         if listy[i] == "c" or listy[i] == "C":
             listy.pop(i)
     return ("".join(listy))
