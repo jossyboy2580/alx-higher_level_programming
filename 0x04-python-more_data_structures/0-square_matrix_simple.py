eab2efef273f89fedf6f9be53892d916e5cf02ff
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-21 02:21:30 +0100

    making  the square of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_matrix.append(list(map(lambda x: x ** 2, row)))
     return new_matrix
+
+if __name__ == "__main__":
+    square_matrix_simple(matrix)
