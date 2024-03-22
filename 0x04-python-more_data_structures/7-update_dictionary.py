bc0c38458596e9bc02ff8e749d1455dc8f50fb2f
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-22 21:48:31 +0100

    updating a dictionary has never been easier

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
-        new_dict = a_dictionary
-        new_dict[key] = value
-        return (new_dict)
+    new_dict = a_dictionary
+    new_dict[key] = value
+    return (new_dict)
