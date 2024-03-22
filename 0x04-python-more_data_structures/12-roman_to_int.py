04a46c909c441a234b5aa98a705dc32d6059d1d0
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-22 22:34:04 +0100

    pycode issues for the roman numerals convertor fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_map = {"M": 1000,
-                "D": 500,
-                "C": 100,
-                "L": 50,
-                "X": 10,
-                "V": 5,
-                "I": 1
-            }
-    if not roman_string or type(roman_string) != str:
+    roman_map = {"M": 1000, "D": 500, "C": 100, "L": 50,
+                 "X": 10, "V": 5, "I": 1}
+    if not roman_string or str not isinstance(roman_string):
         return (0)
     prev = None
     current = None
