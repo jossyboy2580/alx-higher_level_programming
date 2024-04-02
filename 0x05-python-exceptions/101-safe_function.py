5dcdeec2cab3f854cb16f3b8209050349ab7e55b
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-02 23:15:45 +0100

    safe function call

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args)
+        print("{}".format(result))
+    except Exception as exc:
+        result = None
+        print("Exception: {}".format(exc), file=sys.stderr)
+    finally:
+        return (result)
