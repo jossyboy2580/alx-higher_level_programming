c47c5f67b5b4d27819d2e5c9317421e9071ed64a
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-04 17:03:12 -0700

    a class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     A class that defines a square object
     """
     def __init__(self, size=0):
-        if type(size) != int:
+        if size is int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
