2d259906726b8124ded63564d91def86675cb266
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-08 23:36:37 +0100

    some doctests have been added to the file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,11 +15,15 @@ class Rectangle:
     A class that defines a rectangle object
     """
 
-    def __init__(self, width):
+    def __init__(self, width=0, height=0):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be an integer")
         self.__width = width
 
     def width(self):
