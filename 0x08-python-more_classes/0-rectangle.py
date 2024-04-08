a4ad9cfbf007fa5972485a669fa3efd1fb64a00e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-08 23:14:33 +0100

    a rectangle object

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A Module that contains the definition of a class called Rectangle
+along with various functions
+"""
+
+class Rectangle:
+    """
+    A class that defines a rectangle object
+    """
+    pass
