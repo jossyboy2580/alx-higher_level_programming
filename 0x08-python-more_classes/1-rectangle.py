b99699eb92386fd68068e888695974c8356c2516
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-08 23:24:19 +0100

    Our rectangle just got a width and also a method to retrieve/access it

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,13 +9,13 @@ class Rectangle:
     """
     A class that defines a rectangle object
     """
-    
+
     def __init__(self, width):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
-    
+
     def width(self):
         return (self.__width)
