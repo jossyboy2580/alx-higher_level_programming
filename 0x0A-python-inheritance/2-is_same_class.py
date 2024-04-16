4a6170b5ec93af5554bb2bc1b29cd3c4e900d7ce
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-15 20:47:55 -0700

    instanve or child, whichever

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -10,7 +10,7 @@ def is_same_class(obj, a_class):
     this function ascertains if obj is an instance
     of a_class
     """
-    if obj is a_class:
+    if type(obj) == a_class:
         return (True)
     else:
         return (False)
