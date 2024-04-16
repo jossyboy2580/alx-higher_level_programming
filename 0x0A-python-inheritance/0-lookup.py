663fa091ea7881721f64b18466b8cd233f0c20e0
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-15 19:51:59 -0700

    using the dir function to display names of all attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+A module containing a function that lists all attributes and
+methods of an object
+"""
 def lookup(obj):
     """
     A function that returns the names of available
