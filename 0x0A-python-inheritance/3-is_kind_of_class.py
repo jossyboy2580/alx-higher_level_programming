8469952619f2fcdd08671424d519009e50ed6bc6
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-15 20:37:17 -0700

    instanve or child, whichever

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module containing an instance checker
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if object is an instance or instance of
+    a descendant
+    """
+    if obj is a_class:
+        return (True)
+    else:
+        return (False)
