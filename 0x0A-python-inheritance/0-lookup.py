3fb879cf6294565d79e7f923153b1e8723dd9297
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-15 19:30:15 -0700

    Return all attributes and keys of a function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    A function that returns the names of available
+    attributes and functions in an object
+    """
+    if obj:
+        return (obj.__dict__.keys())
