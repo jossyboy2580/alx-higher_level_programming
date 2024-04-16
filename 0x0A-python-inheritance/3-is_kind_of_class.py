10b08ba7265876434625ad87ed09c0996397f31d
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-15 20:44:45 -0700

    instanve or child, whichever

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -9,7 +9,7 @@ def is_kind_of_class(obj, a_class):
     Check if object is an instance or instance of
     a descendant
     """
-    if obj is a_class:
+    if isinstance(obj, a_class):
         return (True)
     else:
         return (False)
