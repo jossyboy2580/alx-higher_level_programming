045de86a2f514abae5b9ccd75da845f6fde89649
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-17 11:46:07 -0700

    is a subclass of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     Function to chexk if an obj is a
     subclass of a_class
     """
-    return (issubclass(type(obj), a_class) and isinstance(obj, a_class))
+    return (issubclass(type(obj), a_class) and not isinstance(obj, a_class))
