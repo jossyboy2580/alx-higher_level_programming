d52deeb2c700ad08929730e80a455318627f08df
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-17 07:22:25 -0700

    read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,7 @@ A module containing a file opening function
 
 
 def read_file(filename=""):
+    """ A function to reaf files """
     if filename:
         with open(filename, encoding='utf-8') as fp:
             print(fp.read())
