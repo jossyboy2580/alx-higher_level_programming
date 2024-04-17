26d3fdd43b02fcbc3dcb149d7326e827c096e1ac
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-17 07:18:18 -0700

    read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ A module containing a file opening function
 def read_file(filename=""):
     if filename:
         with open(filename, encoding='utf-8') as fp:
-            line = fp.readline()
-            print(line)
+            print(fp.read())
