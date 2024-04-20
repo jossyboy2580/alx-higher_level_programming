d5fd8bf778f329ebeec9dd0651ff7bb0f4547e34
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-20 15:37:33 -0700

    add integer function and a test file along with it

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,8 +9,8 @@ def add_integer(a, b=98):
     """
     A function that adds two integers together
     """
-    if type(a) is not int or float:
-        raise TypeError("a must be and integer")
-    if type(b) is not int or float:
-        raise TypeError("b must be and integer")
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
     return (int(a) + int(b))
