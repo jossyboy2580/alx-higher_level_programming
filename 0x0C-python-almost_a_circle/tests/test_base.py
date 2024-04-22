09cf3aa1b02f52f185c33ae3c29500111774adf4
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-23 00:17:05 +0100

    the base id testing now works

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -2,6 +2,10 @@
 """
 This is a test module for the rectangle class
 """
+import sys
+import os
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_path)
 from models.base import Base
 import unittest
 
@@ -19,7 +23,11 @@ class TestBase(unittest.TestCase):
         b1 = Base()
 
     def test_base_id(self):
-        unittest.assertEqual(b1.id, 1)
+        """
+        Test if the correct attribute is being assigned
+        """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
 
 if __name__ == "__main__":
