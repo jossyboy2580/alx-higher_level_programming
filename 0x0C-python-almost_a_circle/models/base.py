c0a3d66288adc38b2d97d000fbba3d04afd9c793
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-26 06:27:37 -0700

    something

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 A package for models
 """
+import json
 
 
 class Base:
@@ -18,3 +19,17 @@ class Base:
         else:
             Base.__no_objects += 1
             self.id = Base.__no_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        to_json_string is a method that returns the json
+        string representation
+        of list_dictionaries
+        """
+        if not list_dictionaries of len(list_dictionaries) < 1:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
+
+    def save_to_file(cls, list_objs):
+        pass
