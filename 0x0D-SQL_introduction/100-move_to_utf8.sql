3a8cbb124846d6b08a41aaee3db6c3f00402c546
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-05-23 15:39:45 -0700

    change char set

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,16 +1,6 @@
 -- Alter the charset
--- Step 1: Change the character set of the database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Step 2: Select the database
 USE hbtn_0c_0;
-
--- Step 3: Change the character set of each table and its columns
--- Note: This part requires altering each table individually.
--- Below is an example for the `first_table`. Repeat as necessary for other tables.
-
--- Change the default character set of the table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 -- Modify the character set of specific columns
-ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
