a3d96782603662e093915654bd69a12e6c0c1aa5
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-05-23 06:10:53 -0700

    All count for scores

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Get the count of all scores
-SELECT UNIQUE score, COUNT(score) AS number FROM second_table ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
