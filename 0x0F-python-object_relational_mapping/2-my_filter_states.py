267adffcfc38062f7442fb42ce1c87ca7754e660
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-07-27 17:08:30 +0100

    sorted the state querying issue

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ import sys
 import MySQLdb
 
 
-def select_states(user, passwd, db_name, state):
+def select_states(user, passwd, db_name, state=None):
     """
     A function to select all the states in a database
     """
@@ -15,20 +15,22 @@ def select_states(user, passwd, db_name, state):
     db = MySQLdb.connect(host='localhost', port=3306, user=user,
                          passwd=passwd, db=db_name)
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name = '{state}'")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
+    if state:
+        cur.execute(f"SELECT * FROM states WHERE name = '{state}'")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        db.close()
 
 
 if __name__ == '__main__':
     """
     query the passed in argz
     """
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    state = sys.argv[4]
-    select_states(user, passwd, db_name, state)
+    if len(sys.argv) == 5:
+        user = sys.argv[1]
+        passwd = sys.argv[2]
+        db_name = sys.argv[3]
+        state = sys.argv[4]
+        select_states(user, passwd, db_name, state)
