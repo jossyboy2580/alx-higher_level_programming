b97a330d2001d80c07c843ea2da15c8cea3a3484
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-07-27 16:57:10 +0100

    some orm shit going on here

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Select states in a database
+starting with a capital N
+"""
+import sys
+import MySQLdb
+
+
+def select_states(user, passwd, db_name):
+    """
+    A function to select all the states in a database
+    """
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                         passwd=passwd, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    """
+    query the passed in argz
+    """
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    select_states(user, passwd, db_name)
