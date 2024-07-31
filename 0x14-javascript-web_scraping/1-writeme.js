175f47f330069ea7d9fc19535811ca65f2397d0f
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-07-31 14:38:24 +0100

    fetching a page from the internet and logging the status code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,3 +2,19 @@
 /**
  * A script to write some text into a file
  */
+
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.error('Usage: <program> <destination_file> <content>');
+  process.exit(1);
+}
+
+const fileName = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * perform a get request and print the status code
+ */
+
+const request = require('request');
+
+const URL = process.argv[2];
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(response.statusCode);
+  }
+});
